5f639d051a460f91b7206aca4c9a40baa892142d
Clemens Wolff <clewolff@microsoft.com>
2019-02-21 08:54:01 -0500

    Avoid creating new client on every server start

--- app/tools/quickstart.py
+++ app/tools/quickstart.py
@@ -1,4 +1,5 @@
 """Script to boostrap the telemetry server start process"""
+from os import getenv
 from subprocess import check_call
 from sys import executable
 
@@ -8,5 +9,9 @@ from app.tools import run_server
 
 if __name__ == '__main__':
     check_call([executable, '-m', create_db.__name__])
-    check_call([executable, '-m', register_client.__name__])
+
+    ikey = getenv('APPINSIGHTS_INSTRUMENTATIONKEY')
+    if ikey:
+        check_call([executable, '-m', register_client.__name__, '--ikey', ikey])
+
     run_server.main()
